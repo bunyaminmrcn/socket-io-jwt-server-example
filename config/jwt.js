6689cc89eb74a2a91a9673127d764ee62d0f5f28
mrcnbunyamin <mrcnbunyamin@gmail.com>
2022-12-14 11:46:44 +0300

    Clean Code

--- config/jwt.js
+++ config/jwt.js
@@ -10,16 +10,6 @@ const jwt = () => {
       // public routes that don't require authentication
       //'/api/connect_check',
       '/api/auth/login',
-      '/api/auth/query',
-      '/api/auth/ui_login',
-      //'/auth/ui_login_bykey',
-      '/api/machines/query',
-      "/api/auth/password_reset_code",
-      "/api/auth/password_reset",
-      "/api/auth/verify_code",
-      "/api/auth/test",
-      "/api/main/category-versions/findAll",
-      "/api/machines/findBySerial/43aa4b6f-efe7-4863-8559-4082a119cdea"
       //pathToRegexp(),
     ]
   });
